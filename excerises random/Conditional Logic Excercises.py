3fd00fb035de5744cd6c7f4a1f387ecd10cfa7df
ricardoax <ricardogarnicaoax@gmail.com>
2023-10-14 09:06:54 -0600

    css

---
+++ excerises random/Conditional Logic Excercises.py
@@ -0,0 +1,8 @@
+color = input("Whats your favorite color? ")
+
+if color == "blue":
+    print("I like the color blue too")
+
+if color.lower() == "blue":
+    print("This is my favorite color madafuka")
+    
