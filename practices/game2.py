b347bf10258f5ad87d4f3fbff7e21e15bd9dbf44
ricardoax <ricardogarnicaoax@gmail.com>
2023-10-27 13:46:33 -0600

    two

---
+++ practices/game2.py
@@ -0,0 +1,8 @@
+word=("moroni")
+print("Welcome to the word guessing game!")
+
+word=input("Your hint is:_ _ _ _ _")
+while word =="moroni":
+    print("Your guess was not correct.")
+print("Congratulations! You guessed it!") 
+  
