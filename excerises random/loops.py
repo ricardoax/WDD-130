861605ad6dc1b24fb469829ae7e076c448b29fe8
ricardoax <ricardogarnicaoax@gmail.com>
2023-10-31 16:31:38 -0600

    push last

---
+++ excerises random/loops.py
@@ -0,0 +1,14 @@
+items = ["crayon", "scissors", "paper", "glitter glue", "markers", "pens" ]
+
+for item in items:
+    print(f"The item is :{item}")
+
+numbers = range(15)
+for number in range(15):
+    print(number)
+
+for i in range (100,200):
+    print(i)
+
+for i in range (100,200,10):
+    print(i)    
